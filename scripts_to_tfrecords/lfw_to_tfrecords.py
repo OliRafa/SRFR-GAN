1a43bde9b7b1f4d543134d25982ee070b760b542
OliRafa <rafaop.19@gmail.com>
2020-02-04 22:06:30 +0100

    Modified scripts to TFRecords to work with LR images.

--- scripts_to_tfrecords/lfw_to_tfrecords.py
+++ scripts_to_tfrecords/lfw_to_tfrecords.py
@@ -1,11 +1,22 @@
 import os
 import sys
+import logging
 import pathlib
 import tensorflow as tf
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import utility.timing
+from utility.timing import TimingLogger; TimingLogger().start()
+from utility.input_data import split_path
 
-print('--- Setting Functions ---')
+logging.basicConfig(
+    filename='lfw_to_tfrecords.txt',
+    level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
+
+LOGGER.info('--- Setting Functions ---')
+
+def _reduce_resolution(high_resolution_image):
+    return tf.image.resize(high_resolution_image, (28, 28), method='bicubic')
 
 def _bytes_feature(value):
     if isinstance(value, type(tf.constant(0))):
@@ -18,31 +29,33 @@ def _float_feature(value):
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-def image_example(image_string, image_shape, label):
+def image_example(image_string_low_resolution, image_shape, _class_id, _sample_id):
     feature = {
         'height': _int64_feature(image_shape[0]),
         'width': _int64_feature(image_shape[1]),
         'depth': _int64_feature(image_shape[2]),
-        'name': _bytes_feature(label[0].encode('UTF-8')),
-        'sample': _bytes_feature(label[1].encode('UTF-8')),
-        'image_raw': _bytes_feature(image_string),
+        'class_id': _bytes_feature(_class_id),
+        'sample_id': _bytes_feature(_sample_id),
+        'image_low_resolution': _bytes_feature(image_string_low_resolution),
     }
 
     return tf.train.Example(features=tf.train.Features(feature=feature))
 
-print('--- Loading Dataset ---')
-
 data_dir = pathlib.Path('/mnt/hdd_raid/datasets/LFW/lfw-deepfunneled/lfw-deepfunneled')
-
 partial = 1
 total = len(list(data_dir.glob('*/*.jpg')))
 
-with tf.io.TFRecordWriter('/mnt/hdd_raid/datasets/TFRecords/LFW/Raw.tfrecords') as writer:
+LOGGER.info(' Started Recording')
+
+with tf.io.TFRecordWriter('/mnt/hdd_raid/datasets/TFRecords/LFW/Raw_Low_Resolution.tfrecords') as writer:
     for image in list(data_dir.glob('*/*.jpg')):
-        print('Image {}/{}'.format(partial, total))
-        label = image.parts[-2], image.parts[-1]
+        LOGGER.info(f' Image {partial}/{total}')
+        class_id, sample_id = split_path(str(image))
         img = tf.io.read_file(str(image))
-        img_shape = tf.shape(tf.image.decode_jpeg(img)).numpy()
-        tf_example = image_example(img, img_shape, label)
+        img = tf.image.decode_jpeg(img)
+        low_resolution = _reduce_resolution(img)
+        img_shape = tf.shape(low_resolution).numpy()
+        low_resolution = tf.io.encode_jpeg(low_resolution)
+        tf_example = image_example(low_resolution, img_shape, class_id, sample_id)
         writer.write(tf_example.SerializeToString())
         partial += 1
--- scripts_to_tfrecords/vgg_to_tfrecords.py
+++ scripts_to_tfrecords/vgg_to_tfrecords.py
@@ -1,23 +1,26 @@
 import os
 import sys
 import pathlib
+import logging
 import tensorflow as tf
-import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import utility.timing
+from utility.timing import TimingLogger
+from utility.input_data import split_path
 
-metadata = pd.read_csv('/mnt/hdd_raid/datasets/VGGFace2/identity_meta.csv')
+logging.basicConfig(
+    filename='vgg_to_tfrecords.txt',
+    level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
 
-print('--- Loading Metadata ---')
+timing = TimingLogger()
+timing.start()
 
-def update_keys(key): return str.strip(str(key))
+LOGGER.info('--- Setting Functions ---')
 
-metadata = metadata.rename(lambda x: update_keys(x), axis='columns')
-metadata.Gender = metadata.Gender.str.strip()
-metadata.Name = metadata.Name.str.strip()
-metadata.Name = metadata.Name.str.strip('"')
-
-print('--- Setting Functions ---')
+def _reduce_resolution(high_resolution_image):
+    low_resolution_image = tf.image.resize(high_resolution_image, (28, 28), method='bicubic')
+    return tf.image.encode_png(low_resolution_image)
 
 def _bytes_feature(value):
     if isinstance(value, type(tf.constant(0))):
@@ -30,55 +33,54 @@ def _float_feature(value):
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-def image_example(image_string, image_shape, label, metadata):
-    metadata = metadata.loc[metadata['Class_ID'] == label[0]].values
-
+def image_example(image_string_low_resolution, image_string_high_resolution, image_shape, _class_id, _sample_id):
     feature = {
         'height': _int64_feature(image_shape[0]),
         'width': _int64_feature(image_shape[1]),
         'depth': _int64_feature(image_shape[2]),
-        'class_id': _bytes_feature(label[0].encode('UTF-8')),
-        'sample': _bytes_feature(label[1].encode('UTF-8')),
-        'name': _bytes_feature(metadata[0, 1].encode('UTF-8')),
-        'gender': _bytes_feature(metadata[0, -1].encode('UTF-8')),
-        'image_raw': _bytes_feature(image_string),
+        'class_id': _bytes_feature(_class_id),
+        'sample': _bytes_feature(_sample_id),
+        'image_low_resolution': _bytes_feature(image_string_low_resolution),
+        'image_high_resolution': _bytes_feature(image_string_high_resolution),
     }
 
     return tf.train.Example(features=tf.train.Features(feature=feature))
 
-print('--- Loading Test Dataset ---')
-
-data_dir = pathlib.Path('/mnt/hdd_raid/datasets/VGGFace2/test')
-
+timing.start('test')
+data_dir = pathlib.Path('/mnt/hdd_raid/datasets/VGGFace2_Aligned/test')
 partial = 1
 total = len(list(data_dir.glob('*/*.jpg')))
 
 with tf.io.TFRecordWriter(
-    '/mnt/hdd_raid/datasets/TFRecords/VGGFace2/Test_Raw.tfrecords') as writer:
+    '/mnt/hdd_raid/datasets/TFRecords/VGGFace2/Test_Low_Resolution_Raw.tfrecords') as writer:
     for image in list(data_dir.glob('*/*.jpg')):
-        print('Test image: {}/{}'.format(partial, total))
-        label = image.parts[-2], image.parts[-1]
+        LOGGER.info(f' Test Image {partial}/{total}')
+        class_id, sample_id = split_path(str(image))
         img = tf.io.read_file(str(image))
-        img_shape = tf.shape(tf.image.decode_jpeg(img)).numpy()
+        high_resolution = tf.image.decode_jpeg(img)
+        low_resolution = _reduce_resolution(high_resolution)
+        img_shape = tf.shape(high_resolution).numpy()
         #img_array = tf.keras.preprocessing.image.img_to_array(img, dtype=int)
         #img_bytes = tf.io.serialize_tensor(img_array)
-        tf_example = image_example(img, img_shape, label, metadata)
+        tf_example = image_example(low_resolution, img, img_shape, class_id, sample_id)
         writer.write(tf_example.SerializeToString())
         partial += 1
+timing.end('test')
 
-print('--- Loading Train Dataset ---')
-
-data_dir = pathlib.Path('/mnt/hdd_raid/datasets/VGGFace2/train')
-
+timing.start('train')
+data_dir = pathlib.Path('/mnt/hdd_raid/datasets/VGGFace2_Aligned/train')
 partial = 1
 total = len(list(data_dir.glob('*/*.jpg')))
 
-with tf.io.TFRecordWriter('/mnt/hdd_raid/datasets/TFRecords/VGGFace2/Train_Raw.tfrecords') as writer:
+with tf.io.TFRecordWriter('/mnt/hdd_raid/datasets/TFRecords/VGGFace2/Train_Low_Resolution_Raw.tfrecords') as writer:
     for image in list(data_dir.glob('*/*.jpg')):
-        print('Train image: {}/{}'.format(partial, total))
-        label = image.parts[-2], image.parts[-1]
+        LOGGER.info(f' Train Image {partial}/{total}')
+        class_id, sample_id = split_path(str(image))
         img = tf.io.read_file(str(image))
-        img_shape = tf.shape(tf.image.decode_jpeg(img)).numpy()
-        tf_example = image_example(img, img_shape, label, metadata)
+        high_resolution = tf.image.decode_jpeg(img)
+        low_resolution = _reduce_resolution(high_resolution)
+        img_shape = tf.shape(high_resolution).numpy()
+        tf_example = image_example(low_resolution, img, img_shape, class_id, sample_id)
         writer.write(tf_example.SerializeToString())
         partial += 1
+timing.end('train')
