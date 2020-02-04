d4994da7c872937beb7f5a7a83df3c34a3d51f54
OliRafa <rafaop.19@gmail.com>
2020-02-04 12:21:39 +0100

    Created validation methods for LFW.

--- validation/lfw_helper.py
+++ validation/lfw_helper.py
@@ -8,23 +8,26 @@ def calculate_accuracy(threshold, dist, actual_issame):
     predict_issame = np.less(dist, threshold)
     tp = np.sum(np.logical_and(predict_issame, actual_issame))
     fp = np.sum(np.logical_and(predict_issame, np.logical_not(actual_issame)))
-    tn = np.sum(np.logical_and(np.logical_not(predict_issame), np.logical_not(actual_issame)))
+    tn = np.sum(np.logical_and(
+        np.logical_not(predict_issame), np.logical_not(actual_issame)
+    ))
     fn = np.sum(np.logical_and(np.logical_not(predict_issame), actual_issame))
 
-    tpr = 0 if (tp+fn==0) else float(tp) / float(tp+fn)
-    fpr = 0 if (fp+tn==0) else float(fp) / float(fp+tn)
-    acc = float(tp+tn)/dist.size
+    tpr = 0 if (tp + fn == 0) else float(tp) / float(tp + fn)
+    fpr = 0 if (fp + tn == 0) else float(fp) / float(fp + tn)
+    acc = float(tp + tn) / dist.size
     return tpr, fpr, acc
 
 def distance(embeddings1, embeddings2, distance_metric=0):
-    if distance_metric==0:
+    if distance_metric == 0:
         # Euclidian distance
         diff = np.subtract(embeddings1, embeddings2)
-        dist = np.sum(np.square(diff),1)
-    elif distance_metric==1:
+        dist = np.sum(np.square(diff), 1)
+    elif distance_metric == 1:
         # Distance based on cosine similarity
         dot = np.sum(np.multiply(embeddings1, embeddings2), axis=1)
-        norm = np.linalg.norm(embeddings1, axis=1) * np.linalg.norm(embeddings2, axis=1)
+        norm = np.linalg.norm(embeddings1, axis=1) * \
+            np.linalg.norm(embeddings2, axis=1)
         similarity = dot / norm
         dist = np.arccos(similarity) / math.pi
     else:
@@ -35,7 +38,9 @@ def distance(embeddings1, embeddings2, distance_metric=0):
 def calculate_val_far(threshold, dist, actual_issame):
     predict_issame = np.less(dist, threshold)
     true_accept = np.sum(np.logical_and(predict_issame, actual_issame))
-    false_accept = np.sum(np.logical_and(predict_issame, np.logical_not(actual_issame)))
+    false_accept = np.sum(
+        np.logical_and(predict_issame, np.logical_not(actual_issame))
+    )
     n_same = np.sum(actual_issame)
     n_diff = np.sum(np.logical_not(actual_issame))
     val = float(true_accept) / float(n_same)
@@ -52,9 +57,8 @@ def calculate_val(
         distance_metric=0,
         subtract_mean=False
     ):
-    assert embeddings1.shape[0] == embeddings2.shape[0]
-    assert embeddings1.shape[1] == embeddings2.shape[1]
-    nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
+    assert embeddings1.shape == embeddings2.shape
+    nrof_pairs = len(actual_issame)
     nrof_thresholds = len(thresholds)
     k_fold = KFold(n_splits=nrof_folds, shuffle=False)
 
@@ -65,47 +69,68 @@ def calculate_val(
 
     for fold_idx, (train_set, test_set) in enumerate(k_fold.split(indices)):
         if subtract_mean:
-            mean = np.mean(np.concatenate([embeddings1[train_set], embeddings2[train_set]]), axis=0)
+            mean = np.mean(
+                np.concatenate([embeddings1[train_set], embeddings2[train_set]]),
+                axis=0,
+            )
         else:
-          mean = 0.0
-        dist = distance(embeddings1-mean, embeddings2-mean, distance_metric)
-      
+            mean = 0.0
+        dist = distance(embeddings1 - mean, embeddings2 - mean, distance_metric)
+
         # Find the threshold that gives FAR = far_target
         far_train = np.zeros(nrof_thresholds)
         for threshold_idx, threshold in enumerate(thresholds):
-            _, far_train[threshold_idx] = calculate_val_far(threshold, dist[train_set], actual_issame[train_set])
-        if np.max(far_train)>=far_target:
+            _, far_train[threshold_idx] = calculate_val_far(
+                threshold,
+                dist[train_set],
+                actual_issame[train_set],
+            )
+        if np.max(far_train) >= far_target:
             f = interpolate.interp1d(far_train, thresholds, kind='slinear')
             threshold = f(far_target)
         else:
             threshold = 0.0
 
-        val[fold_idx], far[fold_idx] = calculate_val_far(threshold, dist[test_set], actual_issame[test_set])
+        val[fold_idx], far[fold_idx] = calculate_val_far(
+            threshold,
+            dist[test_set],
+            actual_issame[test_set]
+        )
 
     val_mean = np.mean(val)
     far_mean = np.mean(far)
     val_std = np.std(val)
     return val_mean, val_std, far_mean
 
-def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_folds=10, distance_metric=0, subtract_mean=False):
-    assert embeddings1.shape[0] == embeddings2.shape[0]
-    assert embeddings1.shape[1] == embeddings2.shape[1]
-    nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
+def calculate_roc(
+        thresholds,
+        embeddings1,
+        embeddings2,
+        actual_issame,
+        nrof_folds=10,
+        distance_metric=0,
+        subtract_mean=False
+    ):
+    assert embeddings1.shape == embeddings2.shape
+    nrof_pairs = len(actual_issame)
     nrof_thresholds = len(thresholds)
     k_fold = KFold(n_splits=nrof_folds, shuffle=False)
 
-    tprs = np.zeros((nrof_folds,nrof_thresholds))
-    fprs = np.zeros((nrof_folds,nrof_thresholds))
+    tprs = np.zeros((nrof_folds, nrof_thresholds))
+    fprs = np.zeros((nrof_folds, nrof_thresholds))
     accuracy = np.zeros((nrof_folds))
 
     indices = np.arange(nrof_pairs)
 
     for fold_idx, (train_set, test_set) in enumerate(k_fold.split(indices)):
         if subtract_mean:
-            mean = np.mean(np.concatenate([embeddings1[train_set], embeddings2[train_set]]), axis=0)
+            mean = np.mean(
+                np.concatenate([embeddings1[train_set], embeddings2[train_set]]),
+                axis=0,
+            )
         else:
             mean = 0.0
-        dist = distance(embeddings1-mean, embeddings2-mean, distance_metric)
+        dist = distance(embeddings1 - mean, embeddings2 - mean, distance_metric)
 
         # Find the best threshold for the fold
         acc_train = np.zeros((nrof_thresholds))
@@ -126,13 +151,23 @@ def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_fold
                 dist[test_set],
                 actual_issame[test_set]
             )
-        _, _, accuracy[fold_idx] = calculate_accuracy(thresholds[best_threshold_index], dist[test_set], actual_issame[test_set])
+        _, _, accuracy[fold_idx] = calculate_accuracy(
+            thresholds[best_threshold_index],
+            dist[test_set],
+            actual_issame[test_set]
+        )
 
         tpr = np.mean(tprs, 0)
         fpr = np.mean(fprs, 0)
     return tpr, fpr, accuracy
 
-def evaluate(embeddings, actual_issame, nrof_folds=10, distance_metric=0, subtract_mean=False):
+def evaluate(
+        embeddings,
+        actual_issame,
+        nrof_folds=10,
+        distance_metric=0,
+        subtract_mean=False
+    ):
     # Calculate evaluation metrics
     thresholds = np.arange(0, 4, 0.01)
     embeddings1 = embeddings[0::2]
@@ -152,7 +187,7 @@ def evaluate(embeddings, actual_issame, nrof_folds=10, distance_metric=0, subtra
         embeddings1,
         embeddings2,
         np.asarray(actual_issame),
-        1e-3,
+        far_target=1e-3,
         nrof_folds=nrof_folds,
         distance_metric=distance_metric,
         subtract_mean=subtract_mean
--- validation/validate.py
+++ validation/validate.py
@@ -1,26 +1,23 @@
-import os
-import sys
-
 import numpy as np
 import tensorflow as tf
 from scipy import interpolate
 from scipy.optimize import brentq
 from sklearn import metrics
-from input_data import (
-    augment_dataset_without_concatenation,
-    load_dataset,
-    normalize_images,
-)
-from input_data import split_path
+from utility.input_data import split_path
 from training.losses import normalize
 from validation.lfw_helper import evaluate
 
-# _load_pairs dar load no class_id e no sample_name separados
-# Verificar funções no evaluate
-def _load_pairs():
+# Talvez no split_path dentro do _load_pairs tenha que retirar o @tf.function
+def load_lfw_pairs():
+    """Loads the Labeled Faces in the Wild pairs from the file.
+    Output array has only the sample_id, not giving the full path or class_id.
+
+    ### Returns
+        Numpy Array of the pairs, with shape [id_01, id_2, is_same].
+    """
     pairs = []
-    with open('pairs_label.txt', 'r') as f:
-        for line in f.readlines()[1:]:
+    with open('pairs_label.txt', 'r') as pairs_file:
+        for line in pairs_file.readlines()[1:]:
             pair = line.strip().split()
             _, id_01 = split_path(pair[0])
             _, id_02 = split_path(pair[1])
@@ -36,10 +33,9 @@ def _get_single_embedding(model, dataset):
     return normalize(embeddings, name='lfw_normalize_embeddings')
 
 @tf.function
-def _get_embeddings(model, dataset):
+def _get_embeddings(model, dataset, pairs):
     embeddings = []
     is_same_list = []
-    pairs = _load_pairs()
     for id_01, id_02, is_same in pairs:
         id_01_dataset = dataset.filter(lambda x: tf.equal(x[3], id_01))
         id_02_dataset = dataset.filter(lambda x: tf.equal(x[3], id_02))
@@ -51,13 +47,14 @@ def _get_embeddings(model, dataset):
     return embeddings, is_same_list
 
 @tf.function
-def validate_model_on_lfw(model, dataset) -> float:
+def validate_model_on_lfw(model, dataset, pairs) -> float:
     """Validates the given model on the Labeled Faces in the Wild dataset.
 
     ### Parameters
         model: The model to be tested.
         dataset: The Labeled Faces in the Wild dataset, loaded from load_lfw\
  function.
+        pairs: List of LFW pairs, loaded from load_lfw_pairs function.
 
     ### Returns
         (accuracy_mean, accuracy_std, validation_rate, validation_std, far,\
@@ -65,14 +62,9 @@ def validate_model_on_lfw(model, dataset) -> float:
  Validation Standard Deviation, FAR, Area Under Curve (AUC) and Equal Error\
  Rate (EER).
     """
-    
-    embeddings, is_same_list = _get_embeddings(model, dataset)
+    embeddings, is_same_list = _get_embeddings(model, dataset, pairs)
 
     tpr, fpr, accuracy, val, val_std, far = evaluate(embeddings, is_same_list)
     auc = metrics.auc(fpr, tpr)
     eer = brentq(lambda x: 1. - x - interpolate.interp1d(fpr, tpr)(x), 0., 1.)
     return np.mean(accuracy), np.std(accuracy), val, val_std, far, auc, eer
-
-if __name__ == '__main__':
-    pairs = _load_pairs()
-    print(pairs.shape)
