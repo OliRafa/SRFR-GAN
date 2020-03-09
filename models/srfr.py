3f2712a58b446b706f9a2554178843f7c2cf7c07
OliRafa <rafaop.19@gmail.com>
2020-03-09 20:42:03 +0100

    Corrected SRFR model.

--- models/srfr.py
+++ models/srfr.py
@@ -1,7 +1,8 @@
+import tensorflow as tf
 from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv2D, Input, LeakyReLU
 from models.generator import GeneratorNetwork
 from models.resnet import ResNet
-from tensorflow.keras.layers import Conv2D, Input, LeakyReLU
 
 class SRFR(Model):
     def __init__(
@@ -13,29 +14,26 @@ class SRFR(Model):
             num_blocks: int = 23,
             residual_scailing: float = 0.2,
             training: bool = True,
+            input_shape=(28, 28, 3),
         ):
         super(SRFR, self).__init__()
         self._training = training
-        self._natural_input = Input(
-            name='natural_input'
-        )
-        self._natural_conv = Conv2D(
+        self._natural_input = Conv2D(
+            input_shape=input_shape,
             filters=num_filters,
             kernel_size=(3, 3),
             strides=1,
             padding='same',
-            name='natural_conv',
+            name='natural_input',
             activation=LeakyReLU(alpha=0.2),
         )
-        self._synthetic_input = Input(
-            name='synthetic_input',
-        )
-        self._synthetic_conv = Conv2D(
+        self._synthetic_input = Conv2D(
+            input_shape=input_shape,
             filters=num_filters,
             kernel_size=(3, 3),
             strides=1,
             padding='same',
-            name='synthetic_conv',
+            name='synthetic_input',
             activation=LeakyReLU(alpha=0.2),
         )
         self._super_resolution = GeneratorNetwork(
@@ -50,44 +48,36 @@ class SRFR(Model):
             training
         )
 
+    @tf.function
     def _call_evaluating(self, input_tensor):
         outputs = self._natural_input(input_tensor)
-        outputs = self._natural_conv(outputs)
+        outputs = self._natural_input(outputs)
         super_resolution_image = self._super_resolution(outputs)
         embeddings, _ = self._face_recognition(
             super_resolution_image
         )
         return super_resolution_image, embeddings
 
+    def get_weights(self):
+        return self._face_recognition.get_weights()
 
+    @tf.function
     def _call_training(self, synthetic_images, natural_images=None):
         synthetic_outputs = self._synthetic_input(synthetic_images)
-        synthetic_outputs = self._synthetic_conv(synthetic_outputs)
-        synthetic_sr_images = self._super_resolution(
-            synthetic_outputs,
-        )
-        synthetic_embeddings, synthetic_fc_weights = self._face_recognition(
-            synthetic_sr_images
-        )
+        synthetic_sr_images = self._super_resolution(synthetic_outputs)
+        synthetic_embeddings = self._face_recognition(synthetic_sr_images)
         if natural_images:
             natural_outputs = self._natural_input(natural_images)
-            natural_outputs = self._natural_conv(natural_outputs)
-            natural_sr_images = self._super_resolution(
-                natural_outputs,
-            )
-            natural_embeddings, natural_fc_weights = self._face_recognition(
-                natural_sr_images,
-            )
+            natural_sr_images = self._super_resolution(natural_outputs)
+            natural_embeddings = self._face_recognition(natural_sr_images)
             return (
                 synthetic_sr_images,
                 synthetic_embeddings,
-                synthetic_fc_weights,
                 natural_sr_images,
                 natural_embeddings,
-                natural_fc_weights
             )
 
-        return synthetic_sr_images, synthetic_embeddings, synthetic_fc_weights
+        return synthetic_sr_images, synthetic_embeddings
 
     def call(self, input_tensor_01, input_tensor_02=None):
         if self._training:
